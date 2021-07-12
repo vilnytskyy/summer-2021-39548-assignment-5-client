6030a1cf952768c9c371f7b417418271c000bf69
vilnytskyy <vilnytskyy21@gmail.com>
2021-07-12 16:05:30 -0400

    Successfuly exported and imported NewCampusContainer

--- src/components/containers/index.js
+++ src/components/containers/index.js
@@ -1,7 +1,8 @@
 // Barrel file for container components (makes it easier to import into App.js)
 export { default as HomePageContainer } from "./HomePageContainer";
 export { default as AllCampusesContainer } from "./AllCampusesContainer";
-export { default as CampusContainer } from "./CampusContainer";
 export { default as AllStudentsContainer } from "./AllStudentsContainer";
-export { default as StudentContainer } from "./StudentContainer";
+export { default as NewCampusContainer } from "./NewCampusContainer";
 export { default as NewStudentContainer } from "./NewStudentContainer";
+export { default as CampusContainer } from "./CampusContainer";
+export { default as StudentContainer } from "./StudentContainer";
