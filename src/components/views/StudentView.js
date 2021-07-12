8e7e13ec65dc2eaf695d20ab8147dbaa966943b5
vilnytskyy <vilnytskyy21@gmail.com>
2021-07-12 09:22:59 -0400

    Added student image, email, and gpa, and created a helpful message if they don't have a campus

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -1,11 +1,33 @@
+import studentImg from "../../imgs/student.png";
+
 const StudentView = (props) => {
-  const { student } = props;
-  return (
-    <div>
-      <h1>{student.firstname + " " + student.lastname}</h1>
-      <h3>{student.campus.name}</h3>
-    </div>
-  );
+    const { student } = props;
+
+    const checkCampus = () => {
+        if (student.campus === undefined || student.campus === null) {
+            return student.firstname + " " + student.lastname + " is currentlty not attending any campus.";
+        }
+
+        return student.campus.name;
+    }
+
+    return (
+        <div>
+            <div>
+                <img src={studentImg} alt="student by ProSymbols from the Noun Project" />
+            </div>
+            <h1>{student.firstname + " " + student.lastname}</h1>
+            <h3>
+                <label>Campus </label>
+                : {checkCampus()}</h3>
+            <h3>
+                <label>Email </label>
+                : {student.email}</h3>
+            <h3>
+                <label>GPA </label>
+                : {student.gpa}</h3>
+        </div>
+    );
 
 };
 
