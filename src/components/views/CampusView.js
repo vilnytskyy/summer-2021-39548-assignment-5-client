2318a5d664f87b2e676dd431072aecb785549448
vilnytskyy <vilnytskyy21@gmail.com>
2021-07-12 08:12:26 -0400

    Added campus image and address, as well as a helpful message if a campus doesn't have students

--- src/components/views/CampusView.js
+++ src/components/views/CampusView.js
@@ -1,21 +1,45 @@
-
+import campusImg from "../../imgs/campus.png";
 
 const CampusView = (props) => {
-  const {campus} = props;
-  return (
-    <div>      
-      <h1>{campus.name}</h1>
-      <p>{campus.description}</p>
-      <ul>
-      {campus.students.map( student => {
-        let name = student.firstname + " " + student.lastname;
-        return (
-          <li key={student.id}>{name}</li>
-        );
-      })}
-      </ul>
-    </div>
-  );
+    const { campus } = props;
+
+    const checkStudents = () => {
+        let temp =
+            <ul>
+                <li>There are no students at this campus.</li>
+            </ul>
+
+        if (campus.students === undefined) {
+            return temp;
+        }
+
+        if (!campus.students.length) {
+            return temp;
+        }
+
+        return temp =
+            <ul>
+                {campus.students.map(student => {
+                    let name = student.firstname + " " + student.lastname;
+                    return (
+                        <li key={student.id}>{name}</li>
+                    );
+                })}
+            </ul>
+    }
+
+    return (
+        <div>
+            <div>
+                <img src={campusImg} alt="Campus by Massupa  Kaewgahya from the Noun Project" />
+            </div>
+            <h1>{campus.name}</h1>
+            <h2>{campus.address}</h2>
+            <p>{campus.description}</p>
+            <h2>Students</h2>
+            {checkStudents()}
+        </div>
+    );
 
 };
 
