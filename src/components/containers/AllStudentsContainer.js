49296cd106a9b85a00fcc658365125240da4d928
vilnytskyy <vilnytskyy21@gmail.com>
2021-07-12 07:15:39 -0400

    Removed extra div

--- src/components/containers/AllStudentsContainer.js
+++ src/components/containers/AllStudentsContainer.js
@@ -9,42 +9,40 @@
 import { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { 
-  fetchAllStudentsThunk,
-  deleteStudentThunk
+import {
+    fetchAllStudentsThunk,
+    deleteStudentThunk
 } from '../../store/thunks';
 
 import AllStudentsView from '../views/AllStudentsView';
 
 class AllStudentsContainer extends Component {
     componentDidMount() {
-      this.props.fetchAllStudents();
+        this.props.fetchAllStudents();
     }
-    render(){
-        return(
-            <div>
-                <AllStudentsView 
-                  students={this.props.allStudents}
-                  deleteStudent={this.props.deleteStudent}   
-                />
-            </div>
+    render() {
+        return (
+            <AllStudentsView
+                students={this.props.allStudents}
+                deleteStudent={this.props.deleteStudent}
+            />
         )
     }
 }
 
 // Map state to props;
 const mapState = (state) => {
-  return {
-    allStudents: state.allStudents,
-  };
+    return {
+        allStudents: state.allStudents,
+    };
 };
 
 // Map dispatch to props;
 const mapDispatch = (dispatch) => {
-  return {
-    fetchAllStudents: () => dispatch(fetchAllStudentsThunk()),
-    deleteStudent: (studentId) => dispatch(deleteStudentThunk(studentId)),
-  };
+    return {
+        fetchAllStudents: () => dispatch(fetchAllStudentsThunk()),
+        deleteStudent: (studentId) => dispatch(deleteStudentThunk(studentId)),
+    };
 };
 
 export default connect(mapState, mapDispatch)(AllStudentsContainer);
