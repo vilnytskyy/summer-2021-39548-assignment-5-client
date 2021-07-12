6ddd5a9e83919bb2d172e1e07fb7f0387b1a6535
vilnytskyy <vilnytskyy21@gmail.com>
2021-07-12 11:42:36 -0400

    Added new campus thunks, added add campus button, created new campus container and view

---
+++ src/components/containers/NewCampusContainer.js
@@ -0,0 +1,71 @@
+import { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+
+import NewCampusView from '../views/NewCampusView';
+import { addCampusThunk } from '../../store/thunks';
+
+
+class NewCampusContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            address: "",
+            description: "",
+            redirect: false,
+            redirectId: null
+        };
+    }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    handleSubmit = async event => {
+        event.preventDefault();
+
+        let campus = {
+            name: this.state.name,
+            address: this.state.address,
+            description: this.state.description
+        };
+
+        let newCampus = await this.props.addCampus(campus);
+
+        this.setState({
+            name: "",
+            address: "",
+            description: "",
+            redirect: true,
+            redirectId: newCampus.id
+        });
+    }
+
+    componentWillUnmount() {
+        this.setState({ redirect: false, redirectId: null });
+    }
+
+    render() {
+        if (this.state.redirect) {
+            console.log("hey");
+            return (<Redirect to={`/campus/${this.state.redirectId}`} />)
+        }
+        return (
+            <NewCampusView
+                handleChange={this.handleChange}
+                handleSubmit={this.handleSubmit}
+            />
+        );
+    }
+}
+
+const mapDispatch = (dispatch) => {
+    return ({
+        addCampus: (campus) => dispatch(addCampusThunk(campus)),
+    })
+}
+
+export default connect(null, mapDispatch)(NewCampusContainer);
