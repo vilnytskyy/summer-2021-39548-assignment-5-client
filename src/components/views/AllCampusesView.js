a67f896b18238fb41c53910751a51233b9ba4183
vilnytskyy <vilnytskyy21@gmail.com>
2021-07-12 05:33:45 -0400

    Added campus images to view

--- src/components/views/AllCampusesView.js
+++ src/components/views/AllCampusesView.js
@@ -1,27 +1,29 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import campusImg from "../../imgs/campus.png";
 
 const AllCampusesView = (props) => {
-  if (!props.allCampuses.length) {
-    return <div>There are no campuses.</div>;
-  }
+    if (!props.allCampuses.length) {
+        return <div>There are no campuses.</div>;
+    }
 
-  return (
-    <div>
-      {props.allCampuses.map((campus) => (
-        <div key={campus.id}>
-          <Link to={`/campus/${campus.id}`}>
-            <h1>{campus.name}</h1>
-          </Link>
-          <p>{campus.description}</p>
+    return (
+        <div>
+            {props.allCampuses.map((campus) => (
+                <div key={campus.id}>
+                    <img src={campusImg} alt="Campus by Massupa  Kaewgahya from the Noun Project" style={{ height: "100px", width: "auto" }} />
+                    <Link to={`/campus/${campus.id}`}>
+                        <h1>{campus.name}</h1>
+                    </Link>
+                    <p>{campus.description}</p>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
 AllCampusesView.propTypes = {
-  allCampuses: PropTypes.array.isRequired,
+    allCampuses: PropTypes.array.isRequired,
 };
 
 export default AllCampusesView;
